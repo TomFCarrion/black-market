918b2be4b516cbf28bcea525150e8889d3387a8b
Tomas F. Carrion <tomfcarrion@gmail.com>
2022-08-07 23:32:54 -0300

    Revert "Feat/login"

--- jest.config.ts
+++ jest.config.ts
@@ -12,7 +12,7 @@ export default {
   // Runs special logic, such as cleaning up components
   // when using React Testing Library and adds special
   // extended assertions to Jest
-  setupFilesAfterEnv: ['@testing-library/react/cleanup-after-each', '@testing-library/jest-dom/extend-expect'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
 
   // Test spec file resolution pattern
   // Matches parent folder `__tests__` and filename
