8658408658544c695f4a852b229492f2334cfffe
tomFCarrion <tomfcarrion@gmail.com>
2022-08-10 17:47:43 -0300

    feat: [LOGIN] add login form styles

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -22,44 +22,50 @@ const LoginForm = ({ onSubmit, customClass }: LoginFormProps) => {
 
   return (
     <div className={`${customClass} loginForm-container`}>
-      <Logo customClass="logo" size="l" />
-      <Formik
-        initialValues={initialValues}
-        onSubmit={onSubmit}
-        validateOnBlur={false}
-        validateOnChange={false}
-        validateOnMount={false}
-      >
-        {({ values, handleChange, handleSubmit, errors }) => (
-          <form onSubmit={handleSubmit} className="form">
-            <div>
-              <Input
-                name="email"
-                value={values.email}
-                error={errors.email}
-                onChange={handleChange}
-                placeholder="Type your email"
-                label="Email"
-                className="login-form-input"
-              />
-            </div>
-            <div>
-              <Input
-                name="password"
-                value={values.password}
-                error={errors.password}
-                onChange={handleChange}
-                type="password"
-                placeholder="Type your password"
-                label="Password"
-                className="login-form-input"
-              />
-            </div>
-            <Button onClick={handleSubmit} title="Log in" variant={'primary'} />
-            <Button onClick={handleSubmit} title="I forgot my password." variant={'outline'} />
-          </form>
-        )}
-      </Formik>
+      <div className="loginAuth-container">
+        <Logo customClass="logo" size="l" />
+        <Formik
+          initialValues={initialValues}
+          onSubmit={onSubmit}
+          validateOnBlur={false}
+          validateOnChange={false}
+          validateOnMount={false}
+        >
+          {({ values, handleChange, handleSubmit, errors }) => (
+            <form onSubmit={handleSubmit} className="form">
+              <div>
+                <Input
+                  name="email"
+                  value={values.email}
+                  error={errors.email}
+                  onChange={handleChange}
+                  placeholder="Type your email"
+                  label="Email"
+                  className="login-form-input"
+                />
+              </div>
+              <div>
+                <Input
+                  name="password"
+                  value={values.password}
+                  error={errors.password}
+                  onChange={handleChange}
+                  type="password"
+                  placeholder="Type your password"
+                  label="Password"
+                  className="login-form-input"
+                />
+              </div>
+              <Button onClick={handleSubmit} title="Log in" variant={'primary'} />
+              <Button onClick={handleSubmit} title="I forgot my password." variant={'outline'} />
+            </form>
+          )}
+        </Formik>
+      </div>
+      <div className="signup-redirect-container">
+        <label>Don’t have an account?</label>
+        <Button onClick={() => console.log('redirect')} title="Sign up" variant={'outline'} />
+      </div>
     </div>
   );
 };
